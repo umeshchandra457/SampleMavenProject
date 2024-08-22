834f9979bd2cb72d4b18702cb4873adc3b6c7173
umeshchandra457 <umeshchandra457@gmail.com>
2024-08-22 19:25:52 +0100

    Initial commit of Sample Project

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    @Test
+    public void AppTest(  ){
+        System.out.println(" Sample Test1 Maven Class ");
+    }
+
+    @Test
+    public void AppTest2(  ){
+        System.out.println(" Sample Test2 Maven Class ");
+    }
+}
